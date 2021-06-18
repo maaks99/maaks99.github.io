d87d92ad3f17ebf5e806bc814da6b0f5be2e76e4
maaks99 <revn@op.pl>
2021-06-18 15:45:12 +0200

    r

--- my-portfolio/js/main.js
+++ my-portfolio/js/main.js
@@ -1,16 +1,77 @@
-let dropDown = document.getElementById('drop');
-let list = document.querySelector('.list');
+$(document).ready(function () {
 
-const toggleClass = () => {
-    if(list.classList.contains("show")) {
+    // toggleClass
 
-        list.style.display = "inline-block";
-        list.classList.remove("show");
-    } else {
-        list.style.display = "none";
-        list.classList.add("show");
-    }
+    let dropDown = $("#drop");
+    let list = $(".list");
 
-}
+    $(dropDown, list).click(function () {
 
-dropDown.addEventListener('click', toggleClass);
+        if (list.hasClass("show")) {
+
+            list.css("display", "inline-block");
+            list.removeClass("show");
+        } else {
+            list.css("display", "none");
+            list.addClass("show");
+        }
+    })
+
+    // smothScroll
+
+    $('a').click(function () {
+
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear')
+
+    });
+
+    // validate form
+
+    let name = $('#name');
+    let email = $('#email');
+    let phone = $('#phone');
+    let message = $('#message');
+
+    $('#btn-submit').click(function (event) {
+
+        if(name.val().trim() === '') {
+            name.css('border', '1px solid red');
+        } else {
+            name.css('border', '1px solid green');
+        }
+
+        if(email.val().trim() === '') {
+            email.css('border', '1px solid red');
+
+        } else if (!email.val().includes("@")) {
+
+            email.css('border', '1px solid red');
+        }
+        else {
+            email.css('border', '1px solid green');
+        }
+
+        if(phone.val().trim() === '') {
+            phone.css('border', '1px solid red');
+        } else {
+            phone.css('border', '1px solid green');
+        }
+
+        if(message.val().trim() === '') {
+            message.css('border', '1px solid red');
+        } else {
+            message.css('border', '1px solid green');
+        }
+
+
+        if(name.val().trim() === '' || email.val().trim() === '' || !email.val().includes("@") || phone.val().trim() === '' || message.val().trim() === '' ) {
+
+             event.preventDefault();
+        } else {
+            
+            $('#form')[0].reset();
+        }
+       
+    });
+
+});
